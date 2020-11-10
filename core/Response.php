1235b1a6f16fd67d4fd22d8b3b83302b55bf6776
Felipe Rodrigues de Oliveira <feliperodrigues916@gmail.com>
2020-11-09 22:20:37 -0300

    Add JSON support

--- core/Response.php
+++ core/Response.php
@@ -6,5 +6,30 @@ namespace Core;
 
 class Response
 {
+    private array $header = [];
 
+    public function __construct()
+    {
+    }
+
+    public function headers(array $headers)
+    {
+        foreach ($headers as $key => $value)
+        {
+            header($key, $value);
+        }
+
+        return $this;
+    }
+
+    public function status(int $statusCode)
+    {
+        http_response_code($statusCode);
+        return $this;
+    }
+
+    public function json(array $data)
+    {
+        return json_encode($data);
+    }
 }
