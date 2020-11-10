4b39313eadcfd02e1e72add0baa3aec7e2161b64
Felipe Rodrigues de Oliveira <feliperodrigues916@gmail.com>
2020-11-09 22:18:43 -0300

    Minor fixes

--- index.php
+++ index.php
@@ -4,9 +4,10 @@ use Core\Router;
 use Core\Request;
 
 require_once 'vendor/autoload.php';
+require_once 'core/helper/helperFunctions.php';
 
 try {
-    Router::init("app/routes.php")::direct(new Request());
+    echo Router::init("app/routes.php")::direct(new Request());
 } catch (Exception $e) {
     echo $e->getMessage();
 }
